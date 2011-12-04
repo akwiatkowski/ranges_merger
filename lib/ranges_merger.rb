3147774b1241eb3a12879b9b3c8f59b2f280c07a
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-12-04 21:21:01 +0100

    some tests

--- lib/ranges_merger.rb
+++ lib/ranges_merger.rb
@@ -5,27 +5,32 @@ class RangesMerger
     a_to = _array[0][1]
     b_to = _array[1][1]
 
-    # 1. outside each other
-    if (a_to < b_to and a_from < b_from and a_to < b_from) or (a_to > b_to and a_from > b_from and b_to < a_from)
+    # 1A. outside each other A > B
+    if (a_to < b_to and a_from < b_from and a_to < b_from)
       return [[a_from, a_to], [b_from, b_to]]
     end
 
-    # 2. A is inside B
+    # 1B. outside each other B > A
+    if  (a_to > b_to and a_from > b_from and b_to < a_from)
+      return [[b_from, b_to], [a_from, a_to]]
+    end
+
+    # 2A. A is inside B
     if a_from >= b_from and a_to <= b_to
       return [[b_from, b_to]]
     end
 
-    # 3. B is inside A
+    # 2B. B is inside A
     if b_from >= a_from and b_to <= a_to
       return [[a_from, a_to]]
     end
 
-    # 4. overlapping, A < B
+    # 3A. overlapping, A < B
     if a_from < b_from and a_to < b_to and a_to >= b_from
       return [[a_from, b_to]]
     end
 
-    # 4. overlapping, B < A
+    # 3B. overlapping, B < A
     if b_from < a_from and b_to < a_to and b_to >= a_from
       return [[b_from, a_to]]
     end
--- spec/ranges_merger_spec.rb
+++ spec/ranges_merger_spec.rb
@@ -1,31 +1,56 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RangesMerger" do
-  it "should merge 2 overlapping" do
+  it "should merge 2 overlapping ranges" do
     a = [1, 3]
     b = [2, 4]
     result = RangesMerger.two_way_merge([a, b])
     result.should == [[1, 4]]
   end
 
-  it "should merge 2 not overlapping" do
+  it "should merge 2 overlapping ranges" do
+    a = [2, 4]
+    b = [1, 3]
+    result = RangesMerger.two_way_merge([a, b])
+    result.should == [[1, 4]]
+  end
+
+  it "should merge 2 not overlapping ranges" do
+    b = [1, 3]
+    a = [5, 6]
+    # it sorts :)
+    result = RangesMerger.two_way_merge([a, b])
+    result.should == [[1, 3], [5, 6]]
+    result.should_not == [[5, 6], [1, 3]]
+  end
+
+  it "should merge 2 not overlapping ranges" do
     a = [1, 3]
     b = [5, 6]
     result = RangesMerger.two_way_merge([a, b])
     result.should == [[1, 3], [5, 6]]
   end
 
-  it "should merge 2 identical" do
+  it "should merge 2 identical ranges" do
     a = [1, 2]
     b = [1, 2]
     result = RangesMerger.two_way_merge([a, b])
     result.should == [[1, 2]]
   end
 
-  it "should merge 2 overlapping" do
-    a = [2, 4]
-    b = [1, 3]
+  it "should merge 2 ranges, A within B" do
+    a = [1, 5]
+    b = [2, 3]
     result = RangesMerger.two_way_merge([a, b])
-    result.should == [[1, 4]]
+    result.should == [[1, 5]]
   end
+
+  it "should merge 2 ranges, B within A" do
+    b = [1, 5]
+    a = [2, 3]
+    result = RangesMerger.two_way_merge([a, b])
+    result.should == [[1, 5]]
+  end
+
+
 end
