351af3e060bb9362db12266f5e5a828fabc75fd7
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-12-04 22:43:14 +0100

    test and doc

--- spec/ranges_merger_spec.rb
+++ spec/ranges_merger_spec.rb
@@ -77,7 +77,7 @@ describe "RangesMerger" do
   end
 
 
-  it "should merge multiple (2)" do
+  it "should merge multiple (3)" do
     ranges = [
       [2, 10],
       [3, 9],
@@ -87,6 +87,52 @@ describe "RangesMerger" do
     ]
     result = RangesMerger.merge(ranges)
     result.should == [[2, 10]]
+  end
+
+
+  it "should merge multiple (3)" do
+    ranges = [
+      [1, 100],
+      [2, 200],
+      [3, 500],
+      [200, 201],
+      [100, 2001]
+    ]
+    result = RangesMerger.merge(ranges)
+    result.should == [[1, 2001]]
+  end
+
+  it "should do the readme code" do
+    a = [1, 3]
+    b = [2, 4]
+    result = RangesMerger.two_way_merge([a, b])
+    result.should == [[1, 4]]
+
+    a = [1, 3]
+    b = [2, 4]
+    c = [4, 6]
+    result = RangesMerger.merge([a, b, c])
+    result.should == [[1, 6]]
+  end
+
+  it "should merge very big array" do
+    ranges = Array.new
+    (1..1000).each do |i|
+      ranges << [i, i + 10]
+    end
+    result = RangesMerger.merge(ranges)
+    result.should == [[1, 1000 + 10]]
+  end
+
+  it "should merge very big array (2)" do
+    ranges = Array.new
+    (1..1000).each do |i|
+      ranges << [10*i, 10*i + 1]
+    end
+    result = RangesMerger.merge(ranges)
+    result.size.should == 1000
+    result[0][0].should == 10
+    result.last[1].should == 1000*10 + 1
   end
 
 
