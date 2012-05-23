69a3af866b793fc0ea3e27e125c62aa5f4223602
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-05-23 15:08:36 +0200

    algorithm for calculating energy for HomeIO

--- lib/ranges_merger/ranges_merger_energy_calculation.rb
+++ lib/ranges_merger/ranges_merger_energy_calculation.rb
@@ -4,8 +4,25 @@ class RangesMerger
     return 0 if voltages.size == 0 or currents.size == 0
 
     # sort time ranges
-    voltages_ranges = voltages.keys.sort{|a,b| a[0] <=> [b]}
-    current_ranges = currents.keys.sort{|a,b| a[0] <=> [b]}
+    voltages = voltages.sort { |a, b| a[0] <=> b[0] }
+    currents = currents.sort { |a, b| a[0] <=> b[0] }
+    # without checking of overlaps
+
+    # for loop
+    voltage_i = 0
+    current_i = 0
+
+    voltage_first = true
+    voltage_first = false if currents[0][0] < voltages[0][0]
+
+    while voltage_i < voltages.size and current_i < currents.size
+      v = voltages[voltage_i]
+      c = currents[current_i]
+
+      puts v.inspect, c.inspect
+      return 0
+
+    end
 
     #currents = [
     #  [1, 2] => 1,
@@ -16,6 +33,7 @@ class RangesMerger
     #  [1, 2] => 2,
     #  [2, 3] => 1
     #]
+
 
     start_time = voltages
 
